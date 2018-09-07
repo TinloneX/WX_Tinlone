cae9fc78e17fa180df486715f6b720631877516d
unknown <1095312588@qq.com>
2018-09-07 17:29:28 +0800

    audio,textarea相关内容

--- pages/coverview/coverview.js
+++ pages/coverview/coverview.js
@@ -5,69 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
-  play: function (e){
-  this.videoCtx.play()
-},
-
-  pause: function (e){
-  this.videoCtx.pause()
-},
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  play: function(e) {
+    this.videoCtx.play()
   },
 
-  /**
+  pause: function(e) {
+    this.videoCtx.pause()
+  },
+
+   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    this.videoCtx = wx.createVideoContext('myVideo', this)
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    this.videoCtx = wx.createVideoContext('myVideo')
   }
 })
